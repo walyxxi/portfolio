5665024a9a6ae3bd1ffc35dec4864e027bfeb98f
Waliyul Ardy <waliyulardy@gmail.com>
2025-07-31 00:40:07 +0800

    feat: section contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,7 +1,15 @@
 import { motion } from "framer-motion";
 import { ArrowUp } from "lucide-react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaMapLocation } from "react-icons/fa6";
+import { MdEmail, MdWhatsapp } from "react-icons/md";
 
 const Contact = () => {
+  const NAME: string = "Waliyul Ardy";
+  const USERNAME: string = "walyxxi";
+  const WA_NUMBER: string = "6282236857567";
+  const EMAIL: string = "waliyulardy@gmail.com";
+
   return (
     <div
       id="contact"
@@ -14,8 +22,66 @@ const Contact = () => {
         transition={{ duration: 1.5 }}
         className="h-full w-full flex flex-col gap-6 md:gap-10 p-6 md:p-12"
       >
-        <h2 className="text-4xl md:text-6xl">Contact</h2>
-        <div className="h-full overflow-y-auto flex flex-col gap-6 md:gap-10 scroll"></div>
+        <div className="overflow-y-auto flex items-center flex-wrap gap-6 md:gap-12 scroll">
+          <div className="flex w-full md:w-auto flex-col items-center justify-center text-lg">
+            <img
+              src="/images/logo.png"
+              alt="Logo"
+              className="w-40 h-40 rounded-full shadow-lg"
+            />
+            <span className="mt-2 font-bold">{NAME}</span>
+            <span className="-mt-1 text-sm font-extralight">
+              Software Developer
+            </span>
+          </div>
+          <div className="flex flex-col gap-4">
+            <a
+              href={`https://maps.app.goo.gl/q8aNwfHoVbwQYCzSA`}
+              className="flex flex-row gap-3 items-center text-xl"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaMapLocation className="w-6 h-6" />
+              <span>Lombok, Indonesia</span>
+            </a>
+            <a
+              href={`https://wa.me/${WA_NUMBER}`}
+              className="flex flex-row gap-3 items-center text-xl"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <MdWhatsapp className="w-6 h-6" />
+              <span>+{WA_NUMBER}</span>
+            </a>
+            <a
+              href={`mailto:${EMAIL}`}
+              className="flex flex-row gap-3 items-center text-xl"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <MdEmail className="w-6 h-6" />
+              <span>{EMAIL}</span>
+            </a>
+            <a
+              href={`https://linkedin.com/in/${USERNAME}`}
+              className="flex flex-row gap-3 items-center text-xl"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin className="w-6 h-6" />
+              <span>linkedin.com/in/{USERNAME}</span>
+            </a>
+            <a
+              href={`https://github.com/${USERNAME}`}
+              className="flex flex-row gap-3 items-center text-xl"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub className="w-6 h-6" />
+              <span>github.com/{USERNAME}</span>
+            </a>
+          </div>
+        </div>
       </motion.div>
       <div className="absolute bottom-0 w-full flex flex-col">
         <motion.div
